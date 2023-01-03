583c7004550f784ed1b44380d82ebd9e6099a392
jullyrud <94559083+jullyrud@users.noreply.github.com>
2023-01-03 18:33:02 +0100

    2nd

---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,44 @@
+import css from './Profile.module.css'
+import PropTypes from 'prop-types'
+
+export function Profile({avatar, username, tag, location, stats}) {
+    return (
+        <div className={css.profile}>
+      <div className={css.description}>
+        <img
+          src={avatar}
+          alt="User avatar"
+          className={css.avatar}
+        />
+        <p className={css.name}>{username}</p>
+        <p className={css.tag}>@{tag}</p>
+        <p className={css.location}>{location}</p>
+      </div>
+
+  <ul className={css.stats}>
+    <li className={css.stats_item}>
+      <span className={css.label}>Followers</span>
+      <span className="quantity">{stats.followers}</span>
+    </li>
+    <li className={css.stats_item}>
+      <span className={css.label}>Views</span>
+      <span className="quantity">{stats.views}</span>
+    </li>
+    <li className={css.stats_item}>
+      <span className={css.label}>Likes</span>
+      <span className="quantity">{stats.likes}</span>
+    </li>
+  </ul>
+</div>
+   ) 
+} 
+
+Profile.propTypes = {
+    avatar: PropTypes.string,
+    username: PropTypes.string,
+    tag: PropTypes.string,
+    location: PropTypes.string,
+    stats: PropTypes.object,
+};
+
+
